103f135279669af0a9750b84408affab8fe3d661
Matt Farkas <z28farkas@gmail.com>
2013-01-16 21:21:01 -0500

    Upload test

--- OptimizingParallelCompiler/MainForm.Designer.cs
+++ OptimizingParallelCompiler/MainForm.Designer.cs
@@ -98,13 +98,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(917, 594);
+            this.ClientSize = new System.Drawing.Size(917, 573);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Optimizing Parallel Complilers";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
