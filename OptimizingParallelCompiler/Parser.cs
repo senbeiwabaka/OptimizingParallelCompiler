3d5515492d8e99f9c50c4902174832e3a8c20677
Michael Yahner <mjy8@zips.uakron.edu>
2013-01-31 19:32:24 -0500

    added beginning of parsing class

---
+++ OptimizingParallelCompiler/Parser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptimizingParallelCompiler
+{
+    public class Parser
+    {
+        private static List<string> lines;
+
+        public static void Change(ref List<string> rtblines)
+        {
+            lines = rtblines;
+
+
+        }
+    }
+}
