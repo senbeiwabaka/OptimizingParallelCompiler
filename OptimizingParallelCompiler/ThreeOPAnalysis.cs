2730c93aa97e736a6f92302acc91876eda2ca062
Michael Yahner <mjy8@zips.uakron.edu>
2013-02-15 17:56:36 -0500

    added index of location in code

--- OptimizingParallelCompiler/ThreeOPAnalysis.cs
+++ OptimizingParallelCompiler/ThreeOPAnalysis.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace OptimizingParallelCompiler
+﻿namespace OptimizingParallelCompiler
 {
     public class ThreeOPAnalysis
     {
         public string VariableName { get; set; }
         public bool Constant { get; set; }
         public int OperandCount { get; set; }
+        public int Index { get; set; }
 
-        public ThreeOPAnalysis(string vname, bool constant, int operandcount)
+        public ThreeOPAnalysis(string vname, bool constant, int operandcount, int index)
         {
             VariableName = vname;
             Constant = constant;
             OperandCount = operandcount;
+            Index = index;
         }
-
-
     }
 }
