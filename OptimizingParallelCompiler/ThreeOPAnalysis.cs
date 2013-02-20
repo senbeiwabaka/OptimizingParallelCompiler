2c113a4ae74d049cdccccd762e3e9cb914252882
Michael Yahner <mjy8@zips.uakron.edu>
2013-02-20 14:41:15 -0500

    some renaming
    variable type change
    added a defualt parameter

--- OptimizingParallelCompiler/ThreeOPAnalysis.cs
+++ OptimizingParallelCompiler/ThreeOPAnalysis.cs
@@ -1,20 +1,34 @@
-﻿namespace OptimizingParallelCompiler
+﻿using System.Collections.Generic;
+
+namespace OptimizingParallelCompiler
 {
     public class ThreeOPAnalysis
     {
-        public string VariableName { get; set; }
+        public string ConstantName { get; set; }
         public bool Constant { get; set; }
+        public string Value { get; set; }
+
         public int OperandCount { get; set; }
         public int Index { get; set; }
-        public bool ArrayAccess { get; set; }
 
-        public ThreeOPAnalysis(string vName, bool constant, int operandCount, int index, bool arrayAccess)
+        public bool ArrayAccess { get; set; }
+        public List<string> ArrayName { get; set; }
+        public List<string> ArrayVariableName { get; set; }
+
+        public int LetCount { get; set; }
+
+        public ThreeOPAnalysis(string constName, string value, bool constant, int operandCount, int index, bool arrayAccess,
+            List<string> arrayName, List<string> arrayVariableName, int letCount = 1)
         {
-            VariableName = vName;
+            ConstantName = constName;
             Constant = constant;
             OperandCount = operandCount;
             Index = index;
             ArrayAccess = arrayAccess;
+            Value = value;
+            ArrayName = arrayName;
+            ArrayVariableName = arrayVariableName;
+            LetCount = letCount;
         }
     }
 }
