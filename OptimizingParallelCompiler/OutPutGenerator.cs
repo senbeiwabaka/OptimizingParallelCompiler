1467f18347f3790382451b48d66059973cbd470e
Michael Yahner <mjy8@zips.uakron.edu>
2013-03-22 07:39:24 -0400

    start of output generator
    wanted to look at something else

---
+++ OptimizingParallelCompiler/OutPutGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OptimizingParallelCompiler
+{
+    public static class OutPutGenerator
+    {
+
+        public static string[] Parser(List<string> code)
+        {
+            var output = new List<string>();
+
+            return output.ToArray();
+        }
+    }
+
+
+}
