6cf3161b513beae7240b05502fe63154e2cdaacb
Michael Yahner <mjy8@zips.uakron.edu>
2013-03-24 13:09:56 -0400

    to print out information on gui
    this will help debug because you can now run it outside of debug and see
    whatever information you wish to

---
+++ OptimizingParallelCompiler/InformationOutput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OptimizingParallelCompiler
+{
+    public static class InformationOutput
+    {
+        public static RichTextBox MainFormTextBox { get; set; }
+
+        public static void InformationPrint(string print)
+        {
+            MainFormTextBox.AppendText(print + Environment.NewLine);
+        }
+    }
+}
